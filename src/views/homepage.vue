ddf0d99a797b1eb03121c7d9f0cbc476c15c6ffd
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-07-08 16:25:40 +0700

    update ui

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,10 +1,10 @@
 <template>
   <ion-content :fullscreen="false">
-    <div className="p-8 w-full">
+    <div className="p-8 w-full bg-neutral-100">
       <Slider />
       <div>
         <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
           type="button">Dropdown button <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
