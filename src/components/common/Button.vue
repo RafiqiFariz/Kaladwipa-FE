517634617289bbef7d52afab3499d390f2cbcd3d
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-07-10 11:36:10 +0700

    add components

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -2,7 +2,7 @@
     <button type="button" :class="{
         'text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm px-5 py-2.5 text-center w-full': isActive,
         'text-black bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-4 font-medium rounded-md text-sm px-5 py-2.5 text-center w-full': !isActive
-    }">{{name}}</button>
+    }">{{ name }}</button>
 </template>
 <script setup>
 const props = defineProps({
