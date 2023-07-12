5cebdd8dec9a059b1065c377f6a0241931bb4846
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-07-12 12:21:41 +0700

    update header responsif

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -97,12 +97,12 @@
             </div>
             <div class="lg:w-full md:w-8 sxsm:w-full">
               <button type="button"
-                class="flex mr-3 text-sm md:bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                class="flex mr-3 text-sm md:bg-gray-800 rounded-full md:mr-0 md:focus:ring-4 md:focus:ring-gray-300"
                 id="user-menu-button" aria-expanded="false" @click="toggleNavbarDropdown">
                 <span class="sr-only">Open user menu</span>
                 <img class="w-8 h-8 rounded-full md:block sxsm:hidden" src="/resources/icon.png" alt="user photo" />
-                <svg class="w-6 h-6 md:hidden sxsm:block text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                  fill="none" viewBox="0 0 17 14">
+                <svg class="w-6 h-6 md:hidden sxsm:block text-gray-800 dark:text-white" aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
@@ -123,6 +123,14 @@
                     <a href="/profile"
                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
                   </li>
+                  <li class="md:hidden block">
+                    <a href="/jelajahi"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Jelajahi</a>
+                  </li>
+                  <li class="md:hidden block">
+                    <a href="/toko"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Toko</a>
+                  </li>
                   <li>
                     <a href="/settings/profile"
                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Pengaturan
@@ -136,7 +144,30 @@
                     <a href="#"
                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white border-b border-gray-300">Penghasilan</a>
                   </li>
-                  <hr />
+                  <li class="md:hidden block">
+                    <a href="/upload-karya"
+                      class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                      <svg class="w-[14px] h-[14px] text-gray-500 dark:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path
+                          d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                      </svg>
+                      <span class="ml-1 text-md">Unggah Karya Baru</span>
+                    </a>
+                  </li>
+                  <li class="md:hidden block">
+                    <a href="/upload-produk"
+                      class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white border-b border-gray-300">
+                      <svg class="w-[14px] h-[14px] text-gray-800 dark:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                        <path fill="currentColor"
+                          d="M11.045 7.514a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm-4.572 3.072L3.857 15.92h7.949l-1.811-3.37-1.61 2.716-1.912-4.679Z" />
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M6 1v4a1 1 0 0 1-1 1H1m14 12a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2v16ZM11.045 7.514a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM3.857 15.92l2.616-5.333 1.912 4.68 1.61-2.717 1.81 3.37H3.858Z" />
+                      </svg>
+                      <span class="ml-1 text-md">Unggah Produk Baru</span>
+                    </a>
+                  </li>
                   <li>
                     <a href="/wishlist"
                       class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
@@ -164,6 +195,28 @@
                         </defs>
                       </svg>
                       <span class="ml-1 text-md">Koleksiku</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#"
+                      class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                      <svg class="lg:w-6 lg:h-6 sxsm:w-4 sxsm:h-4 text-gray-500 dark:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+                        <path
+                          d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
+                      </svg>
+                      <span class="ml-1 text-md">Notifikasi</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/keranjang"
+                      class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white border-b border-gray-300">
+                      <svg class="lg:w-6 lg:h-6 sxsm:w-4 sxsm:h-4 text-gray-500 dark:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+                        <path
+                          d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
+                      </svg>
+                      <span class="ml-1 text-md">Keranjang</span>
                     </a>
                   </li>
                   <li>
