b483f994fc6232dee2276fa053b2ff92bf2b4d35
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-07-29 16:29:37 +0700

    KAL-7 Buat halaman Penghasilan

--- src/views/IncomePage.vue
+++ src/views/IncomePage.vue
@@ -14,85 +14,16 @@
                     <IncomeCard name="Setengah Tahun ini" :data="10" />
                     <IncomeCard name="Tahun ini" :data="10" />
                 </div>
-                <div :class="{ 'h-3/5': isStats() }" class="flex flex-col">
+                <div :class="{ 'h-3/5': isStats() }" class="flex flex-row">
                     <table class="min-w-full text-sm text-center text-black dark:text-gray-400">
                         <thead
                             class="text-xs text-gray-700 uppercase border-b border-black dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3 text-left">Produk</th>
-                                <th scope="col" class="px-6 py-3">Penjual</th>
-                                <th scope="col" class="px-6 py-3">Dilihat</th>
-                                <th scope="col" class="px-6 py-3">Penjualan</th>
-                                <th scope="col" class="px-6 py-3">Pendapatan</th>
-                                <th scope="col" class="px-6 py-3">
-                                    <div class="mt-8 sm:mt-0">
-                                        <button id="dropdownDefaultButton"
-                                            class="mb-2 inline-flex items-center rounded-lg bg-red-700 px-5 text-center text-sm font-medium text-white py-2.5 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300"
-                                            type="button" @click="toggleDropdown">
-                                            Pilih Kategori
-                                            <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                    stroke-width="2" d="m1 1 4 4 4-4" />
-                                            </svg>
-                                        </button>
-                                        <!-- Dropdown menu -->
-                                        <div id="dropdownHelper" v-if="showDropdown"
-                                            class="absolute z-10 w-60 rounded-lg bg-white shadow top-50 divide-y divide-gray-100 dark:divide-gray-600 dark:bg-gray-700">
-                                            <ul class="p-3 text-sm text-gray-700 space-y-1 dark:text-gray-200"
-                                                aria-labelledby="dropdownHelperButton">
-                                                <li>
-                                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                        <div class="flex h-5 items-center">
-                                                            <input id="helper-checkbox-1"
-                                                                aria-describedby="helper-checkbox-text-1" type="checkbox"
-                                                                value=""
-                                                                class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700" />
-                                                        </div>
-                                                        <div class="ml-2 text-sm">
-                                                            <label for="helper-checkbox-1"
-                                                                class="font-medium text-gray-900 dark:text-gray-300">
-                                                                Tradisional
-                                                            </label>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                        <div class="flex h-5 items-center">
-                                                            <input id="helper-checkbox-2"
-                                                                aria-describedby="helper-checkbox-text-2" type="checkbox"
-                                                                value=""
-                                                                class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700" />
-                                                        </div>
-                                                        <div class="ml-2 text-sm">
-                                                            <label for="helper-checkbox-2"
-                                                                class="font-medium text-gray-900 dark:text-gray-300">
-                                                                Tradisional Modern
-                                                            </label>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                        <div class="flex h-5 items-center">
-                                                            <input id="helper-checkbox-3"
-                                                                aria-describedby="helper-checkbox-text-3" type="checkbox"
-                                                                value=""
-                                                                class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700" />
-                                                        </div>
-                                                        <div class="ml-2 text-sm">
-                                                            <label for="helper-checkbox-3"
-                                                                class="font-medium text-gray-900 dark:text-gray-300">
-                                                                Sketa Karakter Tradisional
-                                                            </label>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </th>
+                                <th scope="col" class="px-6 py-3">Pembeli</th>
+                                <th scope="col" class="px-6 py-3">Affiliasi</th>
+                                <th scope="col" class="px-6 py-3">Total</th>
+                                <th scope="col" class="px-6 py-3">Tanggal Terjual</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -111,18 +42,18 @@
                                     </div>
                                 </th>
                                 <td class="px-6 py-4 text-center">
-                                    <h5>{{ item.penjual }}</h5>
+                                    <h5>{{ item.pembeli }}</h5>
                                 </td>
                                 <td class="px-6 py-4 text-center">
-                                    <h5>{{ item.dilihat }}</h5>
+                                    <h5>{{ item.affiliasi }}</h5>
                                 </td>
                                 <td class="px-6 py-4 text-center">
-                                    <h5>{{ item.penjualan }}</h5>
+                                    <h5>{{ item.total }}</h5>
                                 </td>
                                 <td class="px-6 py-4 text-center">
-                                    <h5>{{ item.pendapatan }}</h5>
+                                    <h5>{{ item.tanggalTerjual }}</h5>
                                 </td>
-                                <td>
+                                <!-- <td>
                                     <router-link :to="{
                                         name: 'affiliate-stats',
                                         params: { id: item.id },
@@ -138,10 +69,68 @@
                                             </svg>
                                         </button>
                                     </router-link>
-                                </td>
+                                </td> -->
                             </tr>
                         </tbody>
                     </table>
+                    <div class="mt-8 sm:mt-0">
+                        <button id="dropdownDefaultButton"
+                            class="w-full mb-2 inline-flex rounded-l text-center text-sm font-medium py-2.5 text-black border-none justify-end items-end"
+                            type="button" @click="toggleDropdown">
+                            <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                <path
+                                    d="M18.85 1.1A1.99 1.99 0 0 0 17.063 0H2.937a2 2 0 0 0-1.566 3.242L6.99 9.868 7 14a1 1 0 0 0 .4.8l4 3A1 1 0 0 0 13 17l.01-7.134 5.66-6.676a1.99 1.99 0 0 0 .18-2.09Z" />
+                            </svg>
+                        </button>
+                        <!-- Dropdown menu -->
+                        <div id="dropdownHelper" v-if="showDropdown"
+                            class="absolute z-10 w-40 rounded-lg bg-white shadow top-50 divide-y divide-gray-100 dark:divide-gray-600 dark:bg-gray-700 right-5">
+                            <ul class="p-3 text-sm text-gray-700 space-y-1 dark:text-gray-200"
+                                aria-labelledby="dropdownHelperButton">
+                                <li>
+                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600" @click="toggleDropdown">
+                                        <div class="ml-2 text-sm">
+                                            <label for="helper-checkbox-1"
+                                                class="font-medium text-gray-900 dark:text-gray-300">
+                                                Hari Ini
+                                            </label>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600" @click="toggleDropdown">
+                                        <div class="ml-2 text-sm">
+                                            <label for="helper-checkbox-2"
+                                                class="font-medium text-gray-900 dark:text-gray-300">
+                                                Minggu Ini
+                                            </label>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600" @click="toggleDropdown">
+                                        <div class="ml-2 text-sm">
+                                            <label for="helper-checkbox-1"
+                                                class="font-medium text-gray-900 dark:text-gray-300">
+                                                Bulan Ini
+                                            </label>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="flex rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600" @click="toggleDropdown">
+                                        <div class="ml-2 text-sm">
+                                            <label for="helper-checkbox-2"
+                                                class="font-medium text-gray-900 dark:text-gray-300">
+                                                Tahun Ini
+                                            </label>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </ion-content>
@@ -164,6 +153,12 @@ export default {
         IonContent,
         IncomeCard
     },
+    mounted() {
+        document.addEventListener("click", this.handleClickOutside);
+    },
+    beforeUnmount() {
+        document.removeEventListener("click", this.handleClickOutside);
+    },
     data() {
         return {
             affiliates: [
@@ -173,10 +168,10 @@ export default {
                     produk:
                         "Digital Seni Modern Tradisional Yae Miko Genshin Impact Dengan Kebaya",
                     link: "https://www.kaladwipa.my.id/a/219312387213",
-                    penjual: "Pandu",
-                    dilihat: 10,
-                    penjualan: 0,
-                    pendapatan: "Rp0,00",
+                    pembeli: "Pandu",
+                    affiliasi: 10,
+                    total: 'Rp.150.123',
+                    tanggalTerjual: "Rabu, 10 November 2023",
                 },
                 {
                     id: 2,
@@ -184,10 +179,10 @@ export default {
                     produk:
                         "Digital Seni Modern Tradisional Yae Miko Genshin Impact Dengan Kebaya",
                     link: "https://www.kaladwipa.my.id/a/219312387213",
-                    penjual: "Pandu",
-                    dilihat: 20,
-                    penjualan: 0,
-                    pendapatan: "Rp0,00",
+                    pembeli: "Pandu",
+                    affiliasi: 20,
+                    total: 'Rp.150.123',
+                    tanggalTerjual: "Rabu, 10 November 2023",
                 },
             ],
             showDropdown: ref(false), // Make the variable reactive
@@ -205,14 +200,16 @@ export default {
         },
         toggleDropdown() {
             this.showDropdown = !this.showDropdown;
-        }
+        },
+        handleClickOutside(event) {
+            const dropdownButton = document.getElementById("dropdownDefaultButton");
+            const dropdownHelper = document.getElementById("dropdownHelper");
+
+            // Check if the click was outside the dropdown
+            if (dropdownButton && !dropdownButton.contains(event.target) && dropdownHelper && !dropdownHelper.contains(event.target)) {
+                this.showDropdown = false;
+            }
+        },
     },
 };
 </script>
-
-
-
-
-
-
-  
